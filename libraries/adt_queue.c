bba0ced5960b9a4420be55b1948aae1d1cd9ad9d
Xián Núñez <144531055+x-nunez@users.noreply.github.com>
2024-04-30 23:38:13 +0200

    write queue code

--- libraries/adt_queue.c
+++ libraries/adt_queue.c
@@ -1,4 +1,87 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
+/*Defining the type of element stored in the queue*/
+typedef int ELM_QUEUE;
+/*Structure for each node in the queue*/
+typedef struct queuenode {
+    ELM_QUEUE data;
+    struct queuenode *next;
+} STNODEQUEUE;
+/*Queue structure*/
+typedef struct {
+    STNODEQUEUE *first;
+    STNODEQUEUE *last;
+} STQUEUE;
+typedef STQUEUE *QUEUE;
+
+void CreateQueue(QUEUE *q) {
+    *q = (QUEUE) malloc(sizeof(STQUEUE));
+    (*q)->first = NULL;
+    (*q)->last = NULL;
+}
+
+int IsQueueEmpty(QUEUE q) {
+    return (q->last == NULL) && (q->first == NULL);
+}
+
+void ExtractFirstElementQueue(QUEUE q, ELM_QUEUE *e) {
+    if (IsQueueEmpty(q)) {
+        printf("\nERROR, queue has no elements\n");
+    }
+    else {
+        *e = (q->first)->data;
+    }
+}
+
+void DestroyFirstElementQueue(QUEUE *q) {
+    STNODEQUEUE *aux;
+
+    if (IsQueueEmpty(*q)) {
+        printf("\nERROR, queue has no elements\n");
+    }
+    else {
+        aux = (*q)->first;
+        (*q)->first = aux->next;
+        if ((*q)->first == NULL) {
+            (*q)->last = NULL;
+        }
+        free(aux);
+    }
+}
+
+void DestroyQueue(QUEUE *q) {
+    while (!IsQueueEmpty(*q)) {
+        DestroyFirstElementQueue(q);
+    }
+    free(*q);
+}
+
+void AddElementQueue(QUEUE *q, ELM_QUEUE e) {
+    STNODEQUEUE *aux;
+
+    aux = (STNODEQUEUE *) malloc(sizeof(STNODEQUEUE));
+    aux->data = e;
+    aux->next = NULL;
+
+    if (IsQueueEmpty(*q)) {
+        (*q)->first = aux;
+    }
+    else {
+        (*q)->last->next = aux;
+    }
+    (*q)->last = aux;
+}
+
+int LengthQueue(QUEUE q) {
+    int length = 0;
+    STNODEQUEUE *aux;
+    aux = q->first;
+
+    while (aux) {
+        aux = aux->next;
+        length++;
+    }
+
+    return length;
+}
--- libraries/adt_queue.h
+++ libraries/adt_queue.h
@@ -1,8 +1,62 @@
 #ifndef ADT_QUEUE_H
 #define ADT_QUEUE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*Defining the type of element stored in the queue*/
+typedef int ELM_QUEUE;
+/*Queue structure*/
+typedef void *QUEUE;
+
+/**
+ * Reserve memory for a data queue with type [ELM_QUEUE].
+ *
+ * @param q pointer to the queue to create.
+ */
+void CreateQueue(QUEUE *q);
+
+/**
+ * Destroys (frees reserved memory) the queue [q] and all the elements it stores.
+ *
+ * @param q pointer to the queue to destroy.
+ */
+void DestroyQueue(QUEUE *q);
+
+/**
+ * Checks if queue [q] is empty.
+ *
+ * @param q queue to check if it is empty.
+ * @return 1 if the queue is empty, 0 if not.
+ */
+int IsQueueEmpty(QUEUE q);
+
+/**
+ * Extract the first element of the queue [q], if any, and stores it in [e], without removing it from the queue.
+ *
+ * @param q queue from which to extract the first element.
+ * @param e pointer to the variable where to store the first element of the queue.
+ */
+void ExtractFirstElementQueue(QUEUE q, ELM_QUEUE *e);
+
+/**
+ * Destroys (frees reserved memory) of the first element in the queue.
+ *
+ * @param q pointer to the queue from which to destroy the first element.
+ */
+void DestroyFirstElementQueue(QUEUE *q);
+
+/**
+ * Adds element [e] to queue [q].
+ *
+ * @param q queue pointer to which to add the element.
+ * @param e element to add.
+ */
+void AddElementQueue(QUEUE *q, ELM_QUEUE e);
+
+/**
+ * Query the length of the queue [q].
+ *
+ * @param q queue to query the length of.
+ * @return integer with the value of the length of the queue.
+ */
+int LengthQueue(QUEUE q);
 
 #endif
