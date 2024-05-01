45d358d0322ee79782031e0571724e64dede50e0
Xián Núñez <144531055+x-nunez@users.noreply.github.com>
2024-05-01 12:20:19 +0200

    write list code

--- libraries/adt_list.c
+++ libraries/adt_list.c
@@ -1,4 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "adt_queue.h"
 
+/*Definition of the type of element stored in the list*/
+typedef struct ELM_LIST{
+    char name[50];
+    char brand[50];
+    char model[50];
+    char location[50];
+    QUEUE print_spooler;
+} ELM_LIST;
+/*Structure for each node in the list*/
+typedef struct listnode {
+    ELM_LIST element;
+    struct listnode *next;
+} STNODELIST;
+typedef STNODELIST *LIST_POSITION;
+/*List structure*/
+typedef struct {
+    LIST_POSITION start;
+    int length;
+    LIST_POSITION end;
+} STLIST;
+typedef STLIST *LIST;
+
+void CreateList(LIST *l) {
+    *l = (LIST) malloc(sizeof(LIST));
+    (*l)->start = (LIST_POSITION) malloc(sizeof(STNODELIST));
+    (*l)->start->next = NULL;
+    (*l)->end = (*l)->start;
+    (*l)->length = 0;
+}
+
+void DestroyList(LIST *l) {
+    (*l)->end = (*l)->start;
+    while ((*l)->end != NULL) {
+        (*l)->end = (*l)->end->next;
+        free((*l)->start);
+        (*l)->start = (*l)->end;
+    }
+    free(*l);
+}
+
+int IsListEmpty(LIST l) {
+    return (l->length == 0);
+}
+
+LIST_POSITION FirstList(LIST l) {
+    return l->start;
+}
+
+LIST_POSITION EndList(LIST l) {
+    return l->end;
+}
+
+LIST_POSITION NextList(LIST l, LIST_POSITION p) {
+    return p->next;
+}
+
+void RetrieveElemList(LIST l, LIST_POSITION p, ELM_LIST *e) {
+    *e = p->next->element;
+}
+
+int LengthList(LIST l) {
+    return l->length;
+}
+
+void InsertElemList(LIST *l, LIST_POSITION p, ELM_LIST e) {
+    LIST_POSITION q = p->next;
+    p->next = (STNODELIST *) malloc(sizeof(STNODELIST));
+    p->next->element = e;
+    p->next->next = q;
+    if (q == NULL) {
+        (*l)->end = p->next;
+    }
+    (*l)->length++;
+}
+
+void DeleteElemList(LIST *l, LIST_POSITION p) {
+    LIST_POSITION q = p->next;
+    p->next = q->next;
+    if (p->next == NULL) {
+        (*l)->end = p;
+    }
+    free(q);
+    (*l)->length--;
+}
+
+void ModifyElemList(LIST *l, LIST_POSITION p, ELM_LIST e) {
+    p->next->element = e;
+}
--- libraries/adt_list.h
+++ libraries/adt_list.h
@@ -1,8 +1,109 @@
 #ifndef ADT_LIST_H
 #define ADT_LIST_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "adt_queue.h"
+
+/*Definition of the type of element stored in the list*/
+typedef struct ELM_LIST{
+    char name[50];
+    char brand[50];
+    char model[50];
+    char location[50];
+    QUEUE print_spooler;
+} ELM_LIST;
+/*Structure for a list position*/
+typedef void *LIST_POSITION;
+/*Structure for the list*/
+typedef void *LIST;
+
+/**
+ * Reserve memory for a data list with type [ELM_LIST].
+ *
+ * @param l pointer to the list to create.
+ */
+void CreateList(LIST *l);
+
+/**
+ * Destroys (frees reserved memory) the list [l] and all the elements it stores.
+ *
+ * @param l pointer to the list to destroy.
+ */
+void DestroyList(LIST *l);
+
+/**
+ * Checks if list [l] is empty.
+ *
+ * @param l list to check if it is empty.
+ * @return 1 if the list is empty, 0 if not.
+ */
+int IsListEmpty(LIST l);
+
+/**
+ * Recover the first position in the list.
+ *
+ * @param l list from which to recover the first position.
+ * @return the first position type [LIST_POSITION] of the list [l].
+ */
+LIST_POSITION FirstList(LIST l);
+
+/**
+ * Recovers the position of the end of the list.
+ *
+ * @param l list from which to recover its ending.
+ * @return the position of the end type [LIST_POSITION] of the list [l].
+ */
+LIST_POSITION EndList(LIST l);
+
+/**
+ * Returns the position next to [p] in list [l].
+ *
+ * @param l list in which to search for the next position.
+ * @param p reference position to return the next.
+ * @return the position following [p].
+ */
+LIST_POSITION NextList(LIST l, LIST_POSITION p);
+
+/**
+ * Retrieves the element stored at position [p].
+ *
+ * @param l list from which to retrieve the element.
+ * @param p position from which to retrieve the element.
+ * @param e pointer to the variable in which to store the retrieved element.
+ */
+void RetrieveElemList(LIST l, LIST_POSITION p, ELM_LIST *e);
+
+/**
+ * Query the length of the list [l].
+ *
+ * @param l list to query the length of.
+ * @return integer with the value of the length of the list.
+ */
+int LengthList(LIST l);
+
+/**
+ * Inserts element [e] in the position following position [p] of the list [l].
+ *
+ * @param l pointer to the list into which the element is to be inserted.
+ * @param p position after which the element will be inserted.
+ * @param e element to insert.
+ */
+void InsertElemList(LIST *l, LIST_POSITION p, ELM_LIST e);
+
+/**
+ * Deletes the element at position [p] from list [l].
+ *
+ * @param l pointer to the list from which the element will be deleted.
+ * @param p position of the element to be deleted.
+ */
+void DeleteElemList(LIST *l, LIST_POSITION p);
+
+/**
+ * Modifies the value of the element stored at position [p] by saving the new element [e].
+ *
+ * @param l pointer to the list from which the element is to be modified.
+ * @param p position of the value to be modified.
+ * @param e new value to save in position [p].
+ */
+void ModifyElemList(LIST *l, LIST_POSITION p, ELM_LIST e);
 
 #endif
